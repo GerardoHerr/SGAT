51a3e522f2f817e5765b2f3cfcccb03b04d75181
Santiago Guachizaca <santiagoguachizaca11@gmail.com>
2025-07-14 16:47:41 -0500

    Implements login and registration UI
    Introduces Login and RegistrarUsuario components.
    Adds basic authentication views in the frontend.
    Updates App.vue to conditionally render navigation based on the current route for authentication pages.
    Moves static assets to the assets directory.

--- backend/sgta/api/views.py
+++ backend/sgta/api/views.py
@@ -1,13 +1,6 @@
 from rest_framework import viewsets
-<<<<<<< HEAD
 from ..core.domain import *
 from .serializers import UsuarioSerializer, AsignaturaSerializer,PeriodoLectivoSerializer
-=======
-from sgta.models import Usuario
-from sgta.models import Asignatura
-from sgta.api.serializers import UsuarioSerializer
-from sgta.api.serializers import AsignaturaSerializer
->>>>>>> sergio
 
 class UsuarioViewSet(viewsets.ModelViewSet):
     queryset = Usuario.objects.all()
@@ -17,12 +10,6 @@ class AsignaturaViewSet(viewsets.ModelViewSet):
     queryset = Asignatura.objects.all()
     serializer_class = AsignaturaSerializer
 
-<<<<<<< HEAD
 class PeriodoLectivoViewSet(viewsets.ModelViewSet):
     queryset = PeriodoLectivo.objects.all()
     serializer_class = PeriodoLectivoSerializer
-=======
-    def perform_create(self, serializer):
-        # Automatically set the user who created the subject
-        serializer.save(registrada_por=self.request.user)
->>>>>>> sergio
