653c0acb5dfedd49c236e5eacd21f017b5e7d2f2
Santiago Guachizaca <santiagoguachizaca11@gmail.com>
2025-07-14 09:14:35 -0500

    Moves components to more relevant directories
    This commit reorganizes the project structure by moving components to more appropriate directories:
    
    - Moves the user registration component to the Autenticacion directory.
    - Moves the subject registration component to the GestionAcademica directory.
    
    This change improves code organization and maintainability. It also removes the default welcome components.

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import RegistrarUsuario from '../components/Usuarios/RegistrarUsuario.vue'
-import RegistrarAsignatura from '../components/RegistrarAsignatura.vue'
+import RegistrarUsuario from '../components/Autenticacion/RegistrarUsuario.vue'
+import RegistrarAsignatura from '../components/GestionAcademica/RegistrarAsignatura.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
